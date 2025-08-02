9d0fd5565f8c06f2fc7680f9785a6dc76076c920
Jakiboy <j.sinnaour.official@gmail.com>
2025-08-02 16:33:02 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Hwdecode",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Jakiboy",
+    description="Huawei router decode utilities",
+    python_requires=">=3.6",
+)
